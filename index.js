ecf9edb216601103c38b2731b8259013accca7d3
Grigory Moshkevich <ilcostuff@gmail.com>
2018-01-05 22:28:46 +0300

    ./.,/.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Функционал галереи
 var bigImage = document.querySelector('.gallery__big-item');
 var smallImages = document.querySelector('.gallery__small-items').getElementsByTagName("img");
 
@@ -11,3 +12,19 @@ for (let i = 0; i < smallImages.length; i++) {
 }
 
 console.log(bigImage);
+
+//Функционал кнопок в "интересном"
+var interestingNavbarLinks = document.querySelectorAll('.interesting__navbar-link');
+var interestingContents = document.querySelectorAll('.interesting__content');
+var indexOfLastVisibleContent = 0;
+
+for (let i = 0; i < interestingNavbarLinks.length; i++) {
+  interestingNavbarLinks[i].addEventListener('click', function() {
+    // console.log(interestingNavbarLinks[i].classList.contains('interesting__content_visible'));
+    interestingNavbarLinks[indexOfLastVisibleContent].classList.remove('interesting__content_visible');
+    interestingNavbarLinks[i].classList.add('interesting__content_visible');
+    indexOfLastVisibleContent = i;
+  })
+}
+
+console.log(interestingNavbarLinks.length);
